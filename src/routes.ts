1d05452be5486cc29c019581edc52641313b3dbe
unknown <Savioanjos2020@gmail.com>
2023-03-17 14:58:08 -0300

    Corriginto controller de category

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
 
 import { CreateCategoryController } from "./controllers/category/createCategoreController";
 import { CreateProductController } from "./controllers/product/createProductController";
 
+import  uploadConfig  from "./config/multer";
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 router.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the API" });
@@ -15,6 +20,6 @@ router.post('/category', new CreateCategoryController().handle)
 
 
 // Rotas product
-router.post('/product', new CreateProductController().handle)
+router.post('/product', upload.single("file"), new CreateProductController().handle)
 
 export { router };
