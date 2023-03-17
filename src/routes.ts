6fab494163bcf84597ee7b31a3fbecb8108b1e72
unknown <Savioanjos2020@gmail.com>
2023-03-17 14:26:26 -0300

    Configurando criação de categoria

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from "express";
 
+import { CreateCategoryController } from "./controllers/category/createCategoreController";
+
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the API" });
 });
+
+
+// Rotas category
+router.post('/category', new CreateCategoryController().handle)
 
 export { router };
---
+++ src/services/category/createCategoryService.ts
@@ -0,0 +1,36 @@
+import prismaClient from "../../prisma";
+interface CategoryRequest {
+    name: string;
+}
+
+ class CreateCategoryService {
+    async execute({ name }: CategoryRequest) {
+
+        //verificar se enviou um nome
+        if(!name) {
+            throw new Error("Nome não informado");
+        }
+
+        //verificar sE A CATEGORIA ja está cadastrada
+        const categoryAlreadyExists = await prismaClient.category.findFirst({
+            where: {
+                name: name
+            }
+        })
+
+        if(categoryAlreadyExists) {
+            throw new Error("Categoria já cadastrada");
+        }
+
+        const category = await prismaClient.category.create({
+            data: {
+                name: name
+            }
+        })
+
+        return  category;
+        
+    }
+ }
+
+ export { CreateCategoryService }
