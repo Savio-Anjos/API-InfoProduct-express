52ff5e41dadee225d1d5ec2429a81396a0ae3724
unknown <Savioanjos2020@gmail.com>
2023-03-17 11:06:53 -0300

    Configurando rotas

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import 'express-async-errors';
 //import cors from 'cors';
 import path from 'path';
 
-//import { router } from './routes'
+import { router } from './routes'
 
 const app = express()
 app.use(express.json());
 //app.use(cors())
 
-const port = process.env.PORT || 3001; 
+const port = process.env.PORT ? Number(process.env.PORT) : 3333; 
 
-//app.use(router);
+app.use(router);
 
 app.use(
     '/files',
